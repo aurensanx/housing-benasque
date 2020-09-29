03acbfe30f4e0385918e0234e880d04f693baab6
aaurensanz <aaurensanz@gmail.com>
2020-09-29 20:20:04 +0200

    Updates

---
+++ precache-manifest.924492aa2fb2c230299f000ea3b318b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d787a22cc04d558e936cddc0bf63e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b61c3649c28d1f837b7",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "2f08d9a836a2fb7ceee8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b61c3649c28d1f837b7",
+    "url": "/static/js/2.0dbec1da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0dbec1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2f08d9a836a2fb7ceee8",
+    "url": "/static/js/main.c67a32d8.chunk.js"
+  },
+  {
+    "revision": "10c9494039de77980e17",
+    "url": "/static/js/runtime-main.cca3ad30.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/housing-benasque/precache-manifest.9682bcb7b93e24fb62c13991c583f680.js"
+  "/precache-manifest.924492aa2fb2c230299f000ea3b318b8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/housing-benasque/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
