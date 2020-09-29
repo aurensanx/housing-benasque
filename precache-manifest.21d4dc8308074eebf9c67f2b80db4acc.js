75ac7bdab0f00472d2272623687724c017936371
aaurensanz <aaurensanz@gmail.com>
2020-09-29 20:03:20 +0200

    Updates

---
+++ precache-manifest.21d4dc8308074eebf9c67f2b80db4acc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b62ecb830871b29a85e5e8be6adb5f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b61c3649c28d1f837b7",
+    "url": "/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "bf4ae112e53b0243fa26",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0b61c3649c28d1f837b7",
+    "url": "/static/js/2.0dbec1da.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0dbec1da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf4ae112e53b0243fa26",
+    "url": "/static/js/main.4144cf40.chunk.js"
+  },
+  {
+    "revision": "10c9494039de77980e17",
+    "url": "/static/js/runtime-main.cca3ad30.js"
+  }
+]);
