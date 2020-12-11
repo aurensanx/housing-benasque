374b2f1555b1595bc8678f56b8139df197129027
aaurensanz <aaurensanz@gmail.com>
2020-12-11 22:10:09 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/housing-benasque/precache-manifest.41a7f97cb1af363e327da8dca327b550.js"
+  "/housing-benasque/precache-manifest.9d930127627d9b30623995738f679321.js"
 );
 
 self.addEventListener('message', (event) => {
