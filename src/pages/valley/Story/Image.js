9982e59ad395dc5e4867dbcd62b4a682acd2f555
aaurensanz <aaurensanz@gmail.com>
2020-12-11 19:42:52 +0100

    Image

--- src/pages/valley/Story/Image.js
+++ src/pages/valley/Story/Image.js
@@ -2,9 +2,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const StyledImage = styled.img`
-  position: absolute;
   object-fit: cover;
   width: 100%;
+  height: 100%;
 `
 
 export const Image = ({ src }) => (
