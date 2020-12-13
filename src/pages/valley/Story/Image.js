9de0d829a8cf11fdd7447b9a1e16295324d82ad3
aaurensanz <aaurensanz@gmail.com>
2020-12-13 14:00:59 +0100

    Add text

--- src/pages/valley/Story/Image.js
+++ src/pages/valley/Story/Image.js
@@ -4,7 +4,7 @@ import styled from 'styled-components'
 const StyledImage = styled.img`
   object-fit: cover;
   width: 100%;
-  height: 100%;
+  height: 100% !important;
 `
 
 export const Image = ({ src }) => (
